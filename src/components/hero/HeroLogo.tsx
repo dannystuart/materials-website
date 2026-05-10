90834b11adaeec30aea0d952fcc24347fe651c1e
dannystuart <zsdan88@googlemail.com>
2026-05-10 22:03:54 +0100

    feat: add HeroLogo component

---
+++ src/components/hero/HeroLogo.tsx
@@ -0,0 +1,9 @@
+import { MaterialsWordmark } from "./icons/MaterialsWordmark";
+
+export function HeroLogo() {
+  return (
+    <div className="hero-logo" data-hero-logo>
+      <MaterialsWordmark className="w-[292px] h-auto text-white" />
+    </div>
+  );
+}
